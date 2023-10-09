6345ba011e952ca47e20852fa14ad8e81ba17a19
tlepic <lepicth@gmail.com>
2023-10-09 23:16:41 +0200

    Add baseline model

---
+++ src/methane/models/baseline.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+import pytorch_lightning as pl
+
+
+class MethaneDetectionModel(pl.LightningModule):
+    def __init__(self):
+        super(MethaneDetectionModel, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(32 * 16 * 16 * 16, 64)
+        self.fc2 = nn.Linear(64, 2)  # 2 classes: emission or non-emission
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.float()
+        y = y.long()
+        y_hat = self.forward(x)
+        target = F.one_hot(y, num_classes=2)
+        criterion = torch.nn.BCEWithLogitsLoss()
+        loss = criterion(y_hat, target.float())
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.float()
+        y = y.long()
+        y_hat = self.forward(x)
+        target = F.one_hot(y, num_classes=2)
+        criterion = torch.nn.BCEWithLogitsLoss()
+        loss = criterion(y_hat, target.float())
+        self.log("val_loss", loss)
+
+    def predict_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.float()
+        y = y.long()
+        y_hat = torch.argmax(self(x), dim=1)
+        return y_hat, y
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
