9441f150b631dacdb9e4d4efcab1ead768fb8c42
tlepic <lepicth@gmail.com>
2023-10-09 23:15:56 +0200

    Create dataset class

---
+++ src/methane/dataset.py
@@ -0,0 +1,27 @@
+from typing import Any
+from torch.utils.data import Dataset
+
+
+class ImageDataset(Dataset):
+    def __init__(self, features, targets, transform=True) -> None:
+        super().__init__()
+        self.features = features
+        self.targets = targets
+        self.transform = transform
+
+    def __getitem__(self, index) -> Any:
+        targets = self.targets[index]
+        features = self.features[index]
+
+        if self.transform:
+            features = reshape_transform(features)
+
+        return [features, targets]
+
+    def __len__(self):
+        return len(self.targets)
+
+
+def reshape_transform(x):
+    x = x.view(1, x.shape[0], x.shape[1])  # Reshape to (1, 1, H, W)
+    return x
