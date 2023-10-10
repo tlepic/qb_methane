523bca838fd655e2ecf0187d4a495ceccdf6efeb
tlepic <lepicth@gmail.com>
2023-10-10 10:03:19 +0200

    Refactor the package

--- src/methane/utils.py
+++ src/methane/utils.py
@@ -1,68 +1,6 @@
 import os
-import pathlib
-
-import numpy as np
-import pandas as pd
-import tifffile as tiff
 import torch.nn as nn
 import torch.nn.init as init
-from tqdm import tqdm
-
-# Use a context manager (with statement) to open the TIFF file
-
-
-def load_train(dir_name):
-    data_dir = pathlib.Path(dir_name) / "train_data"
-
-    X_train = []
-    y_train = []
-
-    df_train = pd.read_csv(data_dir / "metadata.csv")
-
-    for sample, plume in tqdm(df_train[["path", "plume"]].values):
-        sample_path = data_dir / sample
-        with tiff.TiffFile(str(sample_path) + ".tif") as tif:
-            _feature = tif.asarray().astype(np.float64)
-
-        X_train.append(_feature)
-        if plume == "yes":
-            y_train.append(1.0)
-        else:
-            y_train.append(0.0)
-
-    print(
-        "Training set specifications\n"
-        "---------------------------\n"
-        f"{len(X_train)} unique samples\n"
-    )
-
-    return np.array(X_train), np.array(y_train)
-
-
-def load_test(dir_name, return_path=False):
-    data_dir = pathlib.Path(dir_name) / "test_data" / "images"
-
-    file_list = [file for file in data_dir.iterdir() if file.is_file()]
-
-    X_test = []
-    y_test = []  # Warning this is used only for compatibility
-
-    for sample in tqdm(file_list):
-        with tiff.TiffFile(sample) as tif:
-            _feature = tif.asarray().astype(np.float64)
-
-        X_test.append(_feature)
-        y_test.append(0.0)
-
-    print(
-        "Testing set specifications\n"
-        "---------------------------\n"
-        f"{len(X_test)} unique samples\n"
-    )
-    if return_path:
-        return np.array(X_test), np.array(y_test), os.listdir(data_dir)
-
-    return np.array(X_test), np.array(y_test)
 
 
 def weight_init(m):
