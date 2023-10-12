f0244bd037e4149d945c2911ccf785f466fb871e
tlepic <lepicth@gmail.com>
2023-10-12 10:50:28 +0200

    sort imports

--- main_multiple_seeds.py
+++ main_multiple_seeds.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 import torch
-from methane import ImageDataset, weight_init, seed_everything
+from methane import ImageDataset, seed_everything, weight_init
 from methane.data import load_train
 from methane.models import (
     Gasnet,
