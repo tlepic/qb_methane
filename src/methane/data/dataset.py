ad3413b11694b7345dcf17907b847f3d9f6447d1
tlepic <lepicth@gmail.com>
2023-10-12 19:46:56 +0200

    Add create submission file and correct minor bugs

--- src/methane/data/dataset.py
+++ src/methane/data/dataset.py
@@ -5,9 +5,7 @@ import torchvision.transforms as transforms
 
 
 class ImageDataset(Dataset):
-    def __init__(
-        self, features, targets, transform=True, normalize=None, extra_feature=None
-    ) -> None:
+    def __init__(self, features, targets, transform=True, extra_feature=None) -> None:
         super().__init__()
         self.features = features
         self.targets = targets
@@ -21,13 +19,8 @@ class ImageDataset(Dataset):
         if self.transform:
             features = reshape_transform(features)
 
-        if normalize is not None:
-            features = normalize(features, normalize[0], normalize[1])
-
         if self.extra_feature is not None:
             extra_feature = self.extra_feature[index]
-            if normalize is not None:
-                extra_feature = normalize(extra_feature, normalize[0], normalize[1])
             if self.transform:
                 extra_feature = reshape_transform(extra_feature)
             return [torch.cat([features, extra_feature], dim=0), targets]
@@ -41,8 +34,3 @@ class ImageDataset(Dataset):
 def reshape_transform(x):
     x = x.view(1, x.shape[0], x.shape[1])  # Reshape to (1, H, W)
     return x
-
-
-def normalize(x, mean, std):
-    x = transforms.Normalize(x, mean, std)
-    return
