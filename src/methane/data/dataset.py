7b4bd70466c67bfd5b13b08b2a4ef0868a84a9f5
octavedbc <octave.dbc@gmail.com>
2023-10-12 22:32:06 +0200

    adding docstrings and comments

--- src/methane/data/dataset.py
+++ src/methane/data/dataset.py
@@ -4,6 +4,23 @@ from torch.utils.data import Dataset
 
 
 class ImageDataset(Dataset):
+    """
+    A custom PyTorch dataset for working with image data, designed to be used with the DataLoader class.
+
+    Args:
+        features (list): A list of image features or paths.
+        targets (list): A list of corresponding target labels.
+        transform (bool, optional): If True, applies a transformation to the image features during retrieval.
+                                    Default is True.
+
+    Methods:
+        __getitem__(self, index): Retrieves a single item (feature, target) from the dataset.
+        __len__(self): Returns the total number of items in the dataset.
+
+    Example:
+        >>> dataset = ImageDataset(features, targets, transform=True)
+        >>> dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    """
     def __init__(self, features, targets, transform=True) -> None:
         super().__init__()
         self.features = features
@@ -11,6 +28,23 @@ class ImageDataset(Dataset):
         self.transform = transform
 
     def __getitem__(self, index) -> Any:
+        """
+        A custom PyTorch dataset for working with image data, designed to be used with the DataLoader class.
+
+        Args:
+            features (list): A list of image features or paths.
+            targets (list): A list of corresponding target labels.
+            transform (bool, optional): If True, applies a transformation to the image features during retrieval.
+                                        Default is True.
+
+        Methods:
+            __getitem__(self, index): Retrieves a single item (feature, target) from the dataset.
+            __len__(self): Returns the total number of items in the dataset.
+
+        Example:
+            >>> dataset = ImageDataset(features, targets, transform=True)
+            >>> dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+        """
         targets = self.targets[index]
         features = self.features[index]
 
@@ -20,6 +54,12 @@ class ImageDataset(Dataset):
         return [features, targets]
 
     def __len__(self):
+        """
+        Get the total number of items in the dataset.
+
+        Returns:
+            int: The number of items in the dataset.
+        """
         return len(self.targets)
 
 
--- src/methane/data/utils.py
+++ src/methane/data/utils.py
@@ -1,6 +1,5 @@
 import os
 import pathlib
-
 import numpy as np
 import pandas as pd
 import tifffile as tiff
@@ -8,10 +7,25 @@ import torch.nn as nn
 import torch.nn.init as init
 from tqdm import tqdm
 
-# Use a context manager (with statement) to open the TIFF file
-
-
 def load_train(dir_name):
+    """
+    Load training data from TIFF images and corresponding metadata.
+
+    Args:
+        dir_name (str): The directory containing the training data.
+
+    Returns:
+        tuple: A tuple containing:
+            - np.ndarray: An array of training features (image data).
+            - np.ndarray: An array of training labels (0.0 for no plume, 1.0 for plume).
+
+    Note:
+        This function reads TIFF images and their corresponding labels from the specified directory,
+        converting the labels to numeric values (0.0 or 1.0) based on the 'plume' column in the metadata.
+
+    Example:
+        >>> X_train, y_train = load_train("data_directory")
+    """
     data_dir = pathlib.Path(dir_name) / "train_data"
 
     X_train = []
@@ -40,6 +54,29 @@ def load_train(dir_name):
 
 
 def load_test(dir_name, return_path=False):
+    """
+    Load test data from TIFF images.
+
+    Args:
+        dir_name (str): The directory containing the test data.
+        return_path (bool, optional): If True, returns a list of file paths along with the test data.
+                                      Default is False.
+
+    Returns:
+        tuple: A tuple containing:
+            - np.ndarray: An array of test features (image data).
+            - np.ndarray: An array of dummy test labels (always 0.0, used for compatibility).
+            - list: A list of file names if 'return_path' is True; otherwise, only the features and dummy labels.
+
+    Note:
+        This function reads TIFF images from the specified directory and returns them as features.
+        It also provides the option to return a list of file names if 'return_path' is set to True.
+
+    Example:
+        >>> X_test, y_test = load_test("test_data_directory")
+        # or
+        >>> X_test, y_test, file_paths = load_test("test_data_directory", return_path=True)
+    """
     data_dir = pathlib.Path(dir_name) / "test_data" / "images"
 
     file_list = [file for file in data_dir.iterdir() if file.is_file()]
